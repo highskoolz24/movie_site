55bd13dd01bcb3c998c7e208f468f25c9cbf73fd
Valentine Choi <jhc591@nyu.edu>
2020-02-03 17:29:21 -0500

    Updates

---
+++ precache-manifest.e9d07bc953f62eddcc5e13e65884b5d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003f14511a7ffd027aec047ee3a06150",
+    "url": "/movie_site/index.html"
+  },
+  {
+    "revision": "3a8688c3d8201be4d292",
+    "url": "/movie_site/static/css/main.c8d88b83.chunk.css"
+  },
+  {
+    "revision": "b369c12847ef4b8059ae",
+    "url": "/movie_site/static/js/2.6e9470b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_site/static/js/2.6e9470b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a8688c3d8201be4d292",
+    "url": "/movie_site/static/js/main.84f27384.chunk.js"
+  },
+  {
+    "revision": "f2e15af3170647f7efcc",
+    "url": "/movie_site/static/js/runtime-main.95933363.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_site/static/media/logo.5d5d9eef.svg"
+  }
+]);
